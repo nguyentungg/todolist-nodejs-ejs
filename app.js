d3066033c60824a05e5b852206fa0518483353ae
Nguyen Tung <thanhtung4693@gmail.com>
2021-10-20 13:05:20 +0700

    update environment file

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-require('dotenv').config();
+const dotenv = require("dotenv")
+
+dotenv.config()
 
 const app = express();
 
@@ -16,7 +18,7 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hzzjf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
@@ -131,6 +133,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
   console.log("Server started on port 3000");
 });
